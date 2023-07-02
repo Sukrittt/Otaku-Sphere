bc5929b1cbcedd5e528ef5a6752b86b063542957
Sukrit Saha <sukritsaha27@gmail.com>
2023-07-02 16:22:55 +0530

    initial commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,1 @@
+//next auth authentication
---
+++ src/types/anime.d.ts
@@ -0,0 +1,8 @@
+interface Anime {
+  id: number;
+  title: string;
+  genre: string[];
+  releaseYear: number;
+  director: string;
+  description: string;
+}
