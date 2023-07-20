64a372cc14f7f57ad38477375c76002195ccb249
Sukrit Saha <sukritsaha27@gmail.com>
2023-07-21 00:12:26 +0530

    fix: responsive issue

--- src/app/(site)/statistics/loading.tsx
+++ src/app/(site)/statistics/loading.tsx
@@ -8,6 +8,7 @@ import {
   TableRow,
 } from "@/ui/Table";
 import { Shell } from "@/components/Shell";
+import { ScrollArea } from "@/ui/ScrollArea";
 
 const TableColumns = ["Rank", "Anime", "Director", "Genre", "Rating", "Votes"];
 
@@ -17,28 +18,30 @@ const StatisticsLoading = () => {
       <h1 className="text-4xl text-center font-bold leading-tight tracking-tighter md:text-6xl lg:text-7xl lg:leading-[1.1]">
         Leaderboard
       </h1>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              {TableColumns.map((column, index) => (
-                <TableHead key={index}>{column}</TableHead>
-              ))}
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {Array.from({ length: 10 }).map((_, index) => (
-              <TableRow key={index}>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <TableCell key={index}>
-                    <Skeleton className="h-4 w-20" />
-                  </TableCell>
+      <ScrollArea className="w-full" orientation="horizontal">
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                {TableColumns.map((column, index) => (
+                  <TableHead key={index}>{column}</TableHead>
                 ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, index) => (
+                <TableRow key={index}>
+                  {Array.from({ length: 6 }).map((_, index) => (
+                    <TableCell key={index}>
+                      <Skeleton className="h-4 w-20" />
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </ScrollArea>
     </Shell>
   );
 };
