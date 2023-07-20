33f79dcc3834d0847882d13c365acb82bce0a436
Sukrit Saha <sukritsaha27@gmail.com>
2023-07-20 17:06:04 +0530

    ui: Community card re-structure.

--- src/components/Cards/CommunityCard.tsx
+++ src/components/Cards/CommunityCard.tsx
@@ -16,24 +16,26 @@ const CommunityCard: FC<CommunityCardProps> = ({ community }) => {
       href={`/community/${community.category.toLowerCase()}/${community.id}`}
     >
       <Card className="flex h-full flex-col">
-        <CardHeader className="flex-1">
-          <CardTitle className="line-clamp-1 pb-1">
-            <div className="flex gap-x-2.5 gap-y-1 md:gap-y-0 md:items-center flex-col md:flex-row">
+        <CardHeader className="flex-1 py-5">
+          <CardTitle className="line-clamp-1">
+            <div className="flex gap-2.5">
               <UserAvatar className="h-8 w-8" user={community.creator} />
-              <div className="mt-2 md:mt-0 flex flex-col md:flex-row md:items-center gap-x-2.5">
-                {community.name}
-                <div className="space-x-1">
-                  <span className="text-sm text-muted-foreground">
-                    {formatTimeToNow(new Date(community.createdAt))}
-                  </span>
-                  <span className="text-sm text-muted-foreground">{`#${community.category.toLowerCase()}`}</span>
+              <div className="space-y-2.5">
+                <div className="flex flex-col md:flex-row md:items-center gap-x-2.5">
+                  {community.name}
+                  <div className="space-x-1">
+                    <span className="text-sm text-muted-foreground">
+                      {formatTimeToNow(new Date(community.createdAt))}
+                    </span>
+                    <span className="text-sm text-muted-foreground">{`#${community.category.toLowerCase()}`}</span>
+                  </div>
                 </div>
+                <CardDescription className="font-normal">
+                  {community.description}
+                </CardDescription>
               </div>
             </div>
           </CardTitle>
-          <CardDescription className="line-clamp-2">
-            {community.description}
-          </CardDescription>
         </CardHeader>
       </Card>
     </Link>
