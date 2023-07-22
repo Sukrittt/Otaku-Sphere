af81211d7518ec11dc7ef346d638d9445295e47b
Sukrit Saha <sukritsaha27@gmail.com>
2023-07-22 22:02:25 +0530

    adding vercel analytics

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ThemeProvider } from "@/components/ThemeProvider";
 
@@ -8,13 +9,16 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          {children}
-        </ThemeProvider>
-      </SessionProvider>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+            {children}
+          </ThemeProvider>
+        </SessionProvider>
+      </QueryClientProvider>
+      <Analytics />
+    </>
   );
 };
 
