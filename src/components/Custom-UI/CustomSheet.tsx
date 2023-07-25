02e0b640d2699f16264814e10597eb33b8318a86
Sukrit Saha <sukritsaha27@gmail.com>
2023-07-26 00:49:13 +0530

    ui: customizing watchlist sheet

--- src/components/Custom-UI/CustomSheet.tsx
+++ src/components/Custom-UI/CustomSheet.tsx
@@ -25,22 +25,30 @@ const CustomSheet: FC<CustomSheetProps> = ({ children }) => {
       <SheetTrigger className={cn(buttonVariants(), "w-fit")}>
         {children}
       </SheetTrigger>
-      <SheetContent>
-        <SheetHeader>
-          <SheetTitle>Add anime to watchlist</SheetTitle>
-          <SheetDescription>
-            Add anime to your watchlist according to your choice. Click save
-            when you&rsquo;re done.
+      <SheetContent className="flex flex-col justify-between">
+        <div>
+          <SheetHeader>
+            <SheetTitle>Add anime to watchlist</SheetTitle>
+            <SheetDescription>
+              Add anime to your watchlist according to your choice. Click save
+              when you&rsquo;re done.
+            </SheetDescription>
+          </SheetHeader>
+          <AddAnimeWatchlistForm />
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button form="anime-watchlist-form" type="submit">
+                Save changes
+              </Button>
+            </SheetClose>
+          </SheetFooter>
+        </div>
+        <div className="mt-auto">
+          <SheetDescription className="text-xs">
+            Note: If any of the anime selected is already in your watchlist, it
+            will be ignored.
           </SheetDescription>
-        </SheetHeader>
-        <AddAnimeWatchlistForm />
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button form="anime-watchlist-form" type="submit">
-              Save changes
-            </Button>
-          </SheetClose>
-        </SheetFooter>
+        </div>
       </SheetContent>
     </Sheet>
   );
