f0dbd18f7438e2f4fb94ab5f13aa2efd974a130c
Sukrit Saha <sukritsaha27@gmail.com>
2023-08-01 21:45:45 +0530

    improving initial load time

--- src/components/ServerComponents/Overview.tsx
+++ src/components/ServerComponents/Overview.tsx
@@ -1,7 +1,8 @@
+"use server";
 import { db } from "@/lib/db";
 import { Icons } from "@/components/Icons";
-import { calculateIncreasePercentage } from "@/lib/utils";
 import { Card, CardContent, CardHeader, CardTitle } from "@/ui/Card";
+import { calculateIncreasePercentage } from "@/lib/utils";
 
 const Overview = async () => {
   const currentDate = new Date();
@@ -54,59 +55,47 @@ const Overview = async () => {
     previousMonthCommunityCount,
   ] = await Promise.all(promises);
 
+  const data = [
+    {
+      title: "Total Users",
+      icon: Icons.users,
+      count: userCount,
+      previousMonthCount: previousMonthUserCount,
+    },
+    {
+      title: "Total Animes",
+      icon: Icons.anime,
+      count: animeCount,
+      previousMonthCount: previousMonthAnimeCount,
+    },
+    {
+      title: "Total Communities",
+      icon: Icons.boxes,
+      count: communityCount,
+      previousMonthCount: previousMonthCommunityCount,
+    },
+  ];
+
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 pb-24">
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total Users</CardTitle>
-          <Icons.users className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">
-            +{userCount.toLocaleString()}
-          </div>
-          <p className="text-xs text-muted-foreground">
-            +{calculateIncreasePercentage(userCount, previousMonthUserCount)}%
-            from last month
-          </p>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Total Animes</CardTitle>
-          <Icons.anime className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">
-            +{animeCount.toLocaleString()}
-          </div>
-          <p className="text-xs text-muted-foreground">
-            +{calculateIncreasePercentage(animeCount, previousMonthAnimeCount)}%
-            from last month
-          </p>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">
-            Total Communities
-          </CardTitle>
-          <Icons.boxes className="h-4 w-4 text-muted-foreground" />
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">
-            +{communityCount.toLocaleString()}
-          </div>
-          <p className="text-xs text-muted-foreground">
-            +
-            {calculateIncreasePercentage(
-              communityCount,
-              previousMonthCommunityCount
-            )}
-            % from last month
-          </p>
-        </CardContent>
-      </Card>
+      {data.map((item, index) => (
+        <Card key={index}>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">{item.title}</CardTitle>
+            <item.icon className="h-4 w-4 text-muted-foreground" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">
+              +{item.count.toLocaleString()}
+            </div>
+            <p className="text-xs text-muted-foreground">
+              +
+              {calculateIncreasePercentage(item.count, item.previousMonthCount)}
+              % from last month
+            </p>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 };
