08db7c5231f0c42b6b88103e2a0fa0460252e089
Sukrit Saha <sukritsaha27@gmail.com>
2023-08-04 22:13:10 +0530

    updating metadata

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "Otaku Sphere",
   description:
-    "Join the ultimate Anime Platform, Next.js 13 based. Explore anime, join communities, manage watchlist, view leaderboards, and more! Open-source hub for enthusiasts.",
+    "Join Next.js 13 based Anime Platform: Explore anime, join communities, manage watchlist, view leaderboards, and more! Open-source hub for enthusiasts.",
   url: "https://otakusphere.in",
 };
 
